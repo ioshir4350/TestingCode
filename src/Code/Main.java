4bca819e4e28244eee8d63e7415d805ff76ffcd3
BT_1N3_25@BKTECH.LOCAL <BT_1N3_25@BKTECH.LOCAL>
2018-10-15 08:41:08 -0400

    Initial commit

---
+++ src/Code/Main.java
@@ -0,0 +1,11 @@
+package Code;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Person p1 = new Person("Deja", "555 132-3253");
+        System.out.println(p1);
+        Person p2 = new Person("Avery", "555 132-6632");
+        System.out.println(p2);
+    }
+}
